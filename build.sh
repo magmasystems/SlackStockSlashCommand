6e38a79fb9d41ab61e368776085f3011555c91ac
magmasystems <magmasystems@gmail.com>
2019-06-20 08:52:59 -0400

    Added the new github.com import to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 go get github.com/nlopes/slack
 go get github.com/lib/pq
+go get github.com/magmasystems/SlackStockSlashCommand
 go build -o bin/application application.go
 cp ./appSettings.json bin
